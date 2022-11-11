c42033bc17d8b8c591b28fcc1069e04dbe1471e5
Jason Alanya <jasonalanya853@gmail.com>
2022-11-11 17:31:07 -0500

    change response 2

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ import routes from "./routes/manage.routes.js";
 
 const app = express();
 
-/*app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
 app.listen(PORT);
-console.log("The server is in the PORT: ", PORT);*/
+console.log("The server is in the PORT: ", PORT);
 
 app.get("/", (req, res) => {
   console.log("Petición recibida");
-  res.status(200).send("<h1>Hola mundo nuevo<h1>");
-});
+  res.status(200).send("<h1>MYSQL<h1>");
+}); /*
 
 app.listen(PORT, () => {
   console.log("The server is in the PORT: ", PORT);
-});
+});*/
